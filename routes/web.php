48fb0ad5d3e9635b28e230eabfe097a5a0e1788e
victor-hugo <hugovco511@gmail.com>
2025-09-05 19:39:51 -0300

    feat: login personalizado

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    return redirect('/pacientes');
 })->name('home');
 
 Route::get('dashboard', function () {
